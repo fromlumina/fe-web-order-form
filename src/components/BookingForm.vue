c591d842782db1f79eb0c98be146f65b52cd7625
fromlumina <from.lumina@gmail.com>
2025-10-30 10:58:46 +0700

    refactor(form): change duration field input type

--- src/components/BookingForm.vue
+++ src/components/BookingForm.vue
@@ -135,7 +135,7 @@ const dateNow = ref<Date | undefined>(undefined)
     <!-- End Col -->
 
     <div class="sm:col-span-9">
-      <input id="af-submit-application-twitter-url" type="text"
+      <input id="af-submit-application-twitter-url" type="number"
              placeholder="60 (dalam menit)"
              class="py-1.5 sm:py-2 px-3 pe-11 block w-full border-gray-200 shadow-2xs sm:text-sm rounded-lg focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600">
     </div>
